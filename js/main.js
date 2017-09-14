4664cda9246b79b9697d08522a76514700ae50cf
Aebischer Aloïs <aaebischer@hublot.ch>
2017-09-14 12:47:13 +0200

    Better structure of filling the arrays

--- js/main.js
+++ js/main.js
@@ -16,32 +16,41 @@ angular.module('freq-spectrum', []).controller('mainController', ['$scope', '$ti
     var index1=0, index2=0, index3=0, serialSeq=0, freqSampling, samples;
     function init() {
         arduinoPort.on('data', function (data) {
+            //Read the frequency sampling
             if(serialSeq==0){
                 freqSampling=data;
                 serialSeq++;
             }
+            //Read the amount of samples
             else if(serialSeq==1){
                 samples=data;
                 serialSeq++;
             }
             else{
-                if(index1==samples && index2==0){
-                    index1=0;
-                    index2=1;
-                }
-                else if(index1==samples/2 && index2==1){
-                    index1=0;
-                    index2=0;
-                    $scope.interfaceData.update=!$scope.interfaceData.update;
-                    $scope.$apply();
-                    $scope.interfaceData.waveData = [];
-                    $scope.interfaceData.frequencyData = [];
-                }
+                //Fill array of wave data
                 if(index2==0){
+                    //Init arrays
+                    if(index1 == 0){
+                        $scope.interfaceData.waveData = [];
+                        $scope.interfaceData.frequencyData = [];
+                    }
                     $scope.interfaceData.waveData.push([index1/freqSampling*1000, data]);
+                    //End of wave data
+                    if (index1==samples-1){
+                        index1=-1
+                        index2=1;
+                    } 
                 }
+                //Fill array of frequency data
                 else if(index2==1){
                     $scope.interfaceData.frequencyData.push([index1/samples*freqSampling, data]);
+                    //End of frequency data
+                    if(index1==samples/2-1){
+                        index1=-1;
+                        index2=0;
+                        $scope.interfaceData.update=!$scope.interfaceData.update;
+                        $scope.$apply();
+                    }
                 }
                 index1++;
             }
